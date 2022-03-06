c85a853d4107b1d0d7e1c15926270928a6941e58
Jlee108 <jbacked@yahoo.com>
2022-03-06 16:31:41 +0000

    All tests pass

--- src/main/java/MicroBlog.java
+++ src/main/java/MicroBlog.java
@@ -4,11 +4,10 @@ class MicroBlog {
     public String truncate(String input) {
         //take the input string and take the first 5 index's and return it
         if (input.length() > 5) {
-            return input.substring(0, 5);
+            return input.codePoints().limit(5)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
         } else {
             return input;
         }
-
-//matches("[A-Za-z]{1}")
     }
 }
